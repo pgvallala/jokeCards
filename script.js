fdaad7f93e452b55e29a3de75bf0719640ca3b2e
pranav157 <pranavgupta.v@gmail.com>
2021-08-09 17:20:01 +0200

    jokes

---
+++ script.js
@@ -0,0 +1,20 @@
+const jokes = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
+
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  fetch("https://icanhazdadjoke.com", config)
+    .then((res) => res.json())
+    .then((data) => {
+      jokes.innerHTML = data.joke;
+    });
+}
